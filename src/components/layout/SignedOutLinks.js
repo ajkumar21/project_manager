9444283e9f515396a4754048620de4e6bffdbeed
AJ Kumar <ajanthan29894@gmail.com>
2019-08-14 17:08:01 +0100

    changed navlink to link in signedoutlinks to eliminate error

--- src/components/layout/SignedOutLinks.js
+++ src/components/layout/SignedOutLinks.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import Button from '@material-ui/core/Button';
 
 const SignedOutLinks = props => {
   return (
     <React.Fragment>
-      <Button style={{ color: 'white' }} component={NavLink} to='/signup'>
+      <Button style={{ color: 'white' }} component={Link} to='/signup'>
         Sign Up
       </Button>
 
-      <Button style={{ color: 'white' }} component={NavLink} to='/signin'>
+      <Button style={{ color: 'white' }} component={Link} to='/signin'>
         Sign In
       </Button>
     </React.Fragment>
