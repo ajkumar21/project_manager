fd3ab125fcf09267ec46049cfa2107a82c84ed44
AJ Kumar <ajanthan29894@gmail.com>
2019-08-14 23:18:56 +0100

    Changed sign in page to use materialUI and use makeStyles - CSS In JS.

--- src/components/auth/SignIn.js
+++ src/components/auth/SignIn.js
@@ -3,47 +3,82 @@ import { signIn } from '../../store/actions/authActions';
 import { connect } from 'react-redux';
 import { Redirect } from 'react-router-dom';
 
+import { makeStyles } from '@material-ui/core/styles';
+import Card from '@material-ui/core/Card';
+import CardActions from '@material-ui/core/CardActions';
+import CardContent from '@material-ui/core/CardContent';
+import Button from '@material-ui/core/Button';
+
+const useStyles = makeStyles({
+  card: {
+    minWidth: 275,
+    maxWidth: 400,
+
+    margin: 'auto',
+    marginTop: '50px'
+  },
+  button: {
+    margin: 'auto',
+    background: 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)',
+    border: 0,
+    borderRadius: 3,
+    boxShadow: '0 3px 5px 2px rgba(255, 105, 135, .3)',
+    color: 'white',
+    height: 48,
+    padding: '0 30px'
+  },
+  title: {
+    fontSize: 14
+  },
+  pos: {
+    marginBottom: 12
+  }
+});
+
 const SignIn = ({ authError, auth, signIn }) => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
+  const classes = useStyles();
+
   // checks if logged in, if so then redirect to dashboard page
   if (auth.uid) return <Redirect to='/' />;
   return (
-    <div className='container'>
-      <h5 className='grey-text text-darken-3'>Sign In</h5>
-      <div className='input-field'>
-        <label htmlFor='email'>Email</label>
-        <input
-          type='email'
-          id='email'
-          value={email}
-          onChange={e => {
-            setEmail(e.target.value);
-          }}
-        />
-      </div>
+    <Card className={classes.card}>
+      <CardContent>
+        <h5 className='grey-text text-darken-3'>Sign In</h5>
+        <div className='input-field'>
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            id='email'
+            value={email}
+            onChange={e => {
+              setEmail(e.target.value);
+            }}
+          />
+        </div>
 
-      <div className='input-field'>
-        <label htmlFor='password'>Password</label>
-        <input
-          type='password'
-          id='password'
-          value={password}
-          onChange={e => setPassword(e.target.value)}
-        />
-      </div>
-
-      <div className='input-field'>
-        <button
+        <div className='input-field'>
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            id='password'
+            value={password}
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+      </CardContent>
+      <CardActions>
+        <Button
+          className={classes.button}
           onClick={() => signIn({ email, password })}
-          className='btn pink lighten-1 z-depth-0'
         >
           Login
-        </button>
+        </Button>
         <div className='red-text center'>{authError ? 'Login Failed' : ''}</div>
-      </div>
-    </div>
+      </CardActions>
+    </Card>
   );
 };
 
